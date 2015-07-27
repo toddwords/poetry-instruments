995706c6137fb7dbcb7b298b1eb1f392e1ab2de7
Todd Anderson <toddwords@gmail.com>
2015-07-27 13:30:01 -0400

    Adds libraries and blank project

---
+++ blank-project/main.js
@@ -0,0 +1,16 @@
+Mousetrap.bind("q", function(){
+	//your code here
+})
+
+Mousetrap.bind("enter", function(){
+	//your code here
+})
+
+Mousetrap.bind("space", function(){
+	//your code here
+})
+
+//bind same function to multiple keys
+Mousetrap.bind(['a','s','d','f'], function(){
+	//your code here
+})
