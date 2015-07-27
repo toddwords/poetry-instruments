869e6f5a9a526d5417d172f8e691841972672d9a
Todd Anderson <toddwords@gmail.com>
2015-07-27 18:24:18 -0400

    adds sample functions

--- blank-project/main.js
+++ blank-project/main.js
@@ -14,3 +14,34 @@ Mousetrap.bind("space", function(){
 Mousetrap.bind(['a','s','d','f'], function(){
 	//your code here
 })
+
+
+//some useful functions, we'll talk about these later
+
+//multiple lines
+function showText (text){
+	$('body').append("<h1>"+text+"</h1>");
+}
+
+//single line
+function showText2 (text){
+	if($('#textZone').length == 0){
+		$('body').append("<h1 id='textZone'>"+text+"</h1>");
+	}
+	else {
+		$('#textZone').text(text);
+	}
+}
+function playSound (id) {
+    sound = $("#" + id)[0]
+    if (sound.ended){sound.currentTime = 0};
+    if (sound.currentTime > 0){
+        sound.currentTime = 0
+    } else {
+        sound.play()
+    }
+}
+
+function setBgImage(fileName){
+	$("html").css('background-image', 'url('+fileName+')'); 
+}
